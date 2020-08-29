09b5e1ef4cd390ceb576f94546e33ee0909a4231
mistydb <mistydbaird@gmail.com>
2020-08-29 10:57:41 -0600

    Add factory function to create two candidates

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-$(document).ready(function(){
-    var politician = {};
-      politician.name = "";
-      politician.electionResults = null;
-      politician.totalVotes = 0;
-});
+var candidate = function(name) 
+{  
+  var politician = {};
+  politician.name = name;
+  politician.electionResults = null;
+  politician.totalVotes = 0;
+  
+  candidate.announceResults = function()
+  {
+    console.log(politician.name + " received a total of " + politician.totalVotes + " votes.");
+  };
+  
+  candidate.announceResults();
+  
+  return candidate;
+};
+
+var candidate1 = candidate("Wonder Woman");
+var candidate2 = candidate("Captain Marvel");
