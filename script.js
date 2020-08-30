0436739c7f3da4c8dedfdd4d05a217d7a967da72
mistydb <mistydbaird@gmail.com>
2020-08-29 18:12:26 -0600

    Determined winner

--- script.js
+++ script.js
@@ -17,14 +17,13 @@ var candidate = function(name)
   return politician;
   
 };
-
+ 
 var diana = candidate("Wonder Woman");
 var carol = candidate("Captain Marvel");
 
 diana.electionResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
 
 carol.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
-
 
 diana.electionResults[9] = 1;
 carol.electionResults[9] = 28;
@@ -40,3 +39,15 @@ carol.tallyVotes();
 
 console.log(diana.totalVotes);
 console.log(carol.totalVotes);
+
+var winner = "";
+
+if (diana.totalVotes > carol.totalVotes) {
+    winner = diana.name;
+  } else if (diana.totalVotes < carol.totalVotes) {
+     winner = carol.name;
+  } else {
+    winner = "Draw.";
+  }
+
+console.log("And the winner is..." + winner + "!!!");
