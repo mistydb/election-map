39b5645cc8bb9667b6cb966f36112f923cce21d2
mistydb <mistydbaird@gmail.com>
2020-08-30 20:48:44 -0600

    Add party colors and assign winner of each state

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-var candidate = function(name) 
+var candidate = function(name, partyColor) 
 {  
   var politician = {};
   politician.name = name;
   politician.electionResults = null;
   politician.totalVotes = 0;
+  politician.partyColor = partyColor;
     
   politician.tallyVotes = function()
   {
@@ -18,12 +19,16 @@ var candidate = function(name)
   
 };
  
-var diana = candidate("Wonder Woman");
-var carol = candidate("Captain Marvel");
+var diana = candidate("Wonder Woman", [132, 17, 11]);
+var carol = candidate("Captain Marvel", [245, 141, 136]);
+
+console.log(diana.name + "'s Party Color is..." + diana.partyColor);
+console.log(carol.name + "'s Party color is... " + carol.partyColor);
 
 diana.electionResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
 
 carol.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
+
 
 diana.electionResults[9] = 1;
 carol.electionResults[9] = 28;
@@ -34,11 +39,28 @@ carol.electionResults[4] = 38;
 diana.electionResults[43] = 11;
 carol.electionResults[43] = 27;
 
+var setStateResults = function(state){
+ 
+    theStates[state].winner = null;
+ 
+    if (diana.electionResults[state] > carol.electionResults[state]) {
+        
+        theStates[state].winner = diana; 
+ 
+    } else if (diana.electionResults[state] < carol.electionResults[state]) {
+ 
+        theStates[state].winner = carol;
+ 
+    }
+}
+
+
 diana.tallyVotes();
 carol.tallyVotes();
 
 console.log(diana.totalVotes);
 console.log(carol.totalVotes);
+
 
 var winner = "";
 
@@ -51,3 +73,4 @@ if (diana.totalVotes > carol.totalVotes) {
   }
 
 console.log("And the winner is..." + winner + "!!!");
+
